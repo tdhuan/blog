92c27c6e4fe5d9e44d8821315a1c13954a4bb30f
tdhuan <dinhhuantran3010@gmail.com>
2026-08-12 16:26:45 +0700

    chore: set up typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,17 @@
 import eslintPluginAstro from "eslint-plugin-astro"
+import * as astroParser from "astro-eslint-parser"
+import tsParser from "@typescript-eslint/parser"
 
 export default [
   ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".astro"],
+      },
+    }
+  },
 ]
